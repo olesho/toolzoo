d1e764b92e067de2494560cc5ae6ba0480b7d4c6
Liliya Semenenko <lilinikcopy@gmail.com>
2025-03-01 13:05:19 -0600

    Create a curent datetime tool

--- tools/time/current.py
+++ tools/time/current.py
@@ -1,20 +1,26 @@
 import os
 from langchain.tools import BaseTool
+from datetime import datetime
+from typing import Dict
+from typing import Dict, Optional
 from pydantic import BaseModel, Field
-import requests
+
+
+class ToolInputSchema(BaseModel):
+    dummy: Optional[str] = Field(None, description="Not used")
+
+    class Config:
+        extra = "forbid"
 
 class CurrentTimeTool(BaseTool):
-    name: str = "current_time_in"
-    description: str = "Get the current time"
+    name: str = "current_time"
+    description: str = "Get the current time in UTC"
+    args_schema: type[BaseModel] = ToolInputSchema
 
-    def _run(self, region: str):
-        """Get current time in specified region"""
-        print("api call...")
-        #"http://worldtimeapi.org/api/timezone/America/Argentina/Cordoba"
-
-        response = requests.get(f"http://worldtimeapi.org/api/timezone/America/Argentina/{region}")
-        return response.content
+    def _run(self, dummy: Optional[str] = None) -> str:
+        current_utc = datetime.utcnow()
+        return current_utc.strftime("%Y-%m-%d %H:%M:%S UTC")
 
 if __name__ == "__main__":
     tool = CurrentTimeTool()
-    print(tool.invoke({"region": "Cordoba"}))
+    print(tool.invoke({}))
